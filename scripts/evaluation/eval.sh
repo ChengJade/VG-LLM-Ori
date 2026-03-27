d312d350e03adf0dedada21290f19b4379b8cd90
jadecheng <36798055+ChengJade@users.noreply.github.com>
2026-03-27 13:37:48 +0000

    Update model path and reduce process count for eval

--- scripts/evaluation/eval.sh
+++ scripts/evaluation/eval.sh
@@ -4,9 +4,9 @@ export LMMS_EVAL_LAUNCHER="accelerate"
 export NCCL_NVLS_ENABLE=0
 benchmark=vsibench # choices: [vsibench, cvbench, blink_spatial]
 output_path=logs/$(TZ="Asia/Shanghai" date "+%Y%m%d")
-model_path=zd11024/vgllm-qa-vggt-4b
+model_path=/mnt/ceph/3d_llm/model/vgllm-3d-vggt-4b
 
-accelerate launch --num_processes=8 -m lmms_eval \
+accelerate launch --num_processes=2 -m lmms_eval \
     --model vgllm \
     --model_args pretrained=$model_path,use_flash_attention_2=true,max_num_frames=32,max_length=12800 \
     --tasks ${benchmark} \
